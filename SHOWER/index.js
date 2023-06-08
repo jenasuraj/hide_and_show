9db6300ade378eca90c168785626538d7db46122
jenasuraj <jenasuraj742@gmail.com>
2023-06-08 12:36:23 +0530

    shower added

---
+++ SHOWER/index.js
@@ -0,0 +1,15 @@
+function presser()
+{
+    alert("button was clicked...");
+  let shower=document.getElementById('shower');
+let name=document.getElementById('name');
+
+if(name.style.display !='none')
+{
+    name.style.display='none';
+}
+else
+{
+    name.style.display='block';
+}
+}
